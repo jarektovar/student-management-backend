90d750798de95262faa2a9b0e5fc7c032ecbcd82
Jsebastian96 <89179453+Jsebastian96@users.noreply.github.com>
2024-07-20 16:19:36 -0500

    commit 11

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -4,30 +4,7 @@ const auth = require('../middlewares/auth');
 const roles = require('../middlewares/roles');
 const Course = require('../models/course');
 const Enrollment = require('../models/enrollment');
-
-router.get('/', auth, roles('admin'), async (req, res) => {
-  const { pageNumber = 1, pageSize = 10 } = req.query;
-  const skip = (pageNumber - 1) * pageSize;
-
-  try {
-    const courses = await Course.find().skip(skip).limit(parseInt(pageSize));
-    const totalCourses = await Course.countDocuments();
-
-    const coursesWithEnrollments = await Promise.all(
-      courses.map(async (course) => {
-        const enrollments = await Enrollment.find({ materia_id: course._id });
-        const enrollmentsCount = enrollments.length;
-        const studentIds = enrollments.map(enrollment => enrollment.estudiante_id);
-        return { ...course.toObject(), enrollmentsCount, studentIds };
-      })
-    );
-
-    res.json({ courses: coursesWithEnrollments, totalCourses });
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
+const Student = require('../models/student'); // Importa el modelo de Student
 
 router.get('/', auth, roles('admin'), async (req, res) => {
   const { pageNumber = 1, pageSize = 10 } = req.query;
@@ -40,9 +17,22 @@ router.get('/', auth, roles('admin'), async (req, res) => {
     const coursesWithEnrollments = await Promise.all(
       courses.map(async (course) => {
         const enrollments = await Enrollment.find({ materia_id: course._id }).lean();
-        const enrollmentsCount = enrollments.length;
+
+        // Obtener los nombres y apellidos de los estudiantes
         const studentIds = enrollments.map(enrollment => enrollment.estudiante_id);
-        return { ...course, enrollmentsCount, studentIds };
+        const students = await Student.find({ _id: { $in: studentIds } });
+
+        const enrollmentsInfo = enrollments.map(enrollment => {
+          const student = students.find(student => student._id.equals(enrollment.estudiante_id));
+          return {
+            nombre: student ? student.nombre_name : 'Desconocido',
+            apellido: student ? student.apellido : 'Desconocido'
+          };
+        });
+
+        const enrollmentsCount = enrollmentsInfo.length;
+
+        return { ...course, enrollmentsCount, enrollmentsInfo };
       })
     );
 
@@ -51,4 +41,5 @@ router.get('/', auth, roles('admin'), async (req, res) => {
     res.status(500).send(error.message);
   }
 });
+
 module.exports = router;
--- src/routes/programs.js
+++ src/routes/programs.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Program = require('../models/program');
+const Faculty = require('../models/faculty');
+const mongoose = require('mongoose');
 
 // Obtener todos los programas
 router.get('/', async (req, res) => {
@@ -12,12 +14,22 @@ router.get('/', async (req, res) => {
   }
 });
 
-
 // Crear un nuevo programa
 router.post('/', async (req, res) => {
   const { nombre_programa, facultad_id } = req.body;
 
   try {
+    // Verificar si facultad_id es un ObjectId válido
+    if (!mongoose.Types.ObjectId.isValid(facultad_id)) {
+      return res.status(400).json({ message: 'facultad_id no es válido' });
+    }
+
+    // Verificar si la facultad existe
+    const existingFaculty = await Faculty.findById(facultad_id);
+    if (!existingFaculty) {
+      return res.status(404).json({ message: 'La facultad no existe' });
+    }
+
     const newProgram = new Program({ nombre_programa, facultad_id });
     await newProgram.save();
     res.status(201).json(newProgram);
