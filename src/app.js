7ffb170a8cc5565277902d952a55576f365ee698
Jsebastian96 <89179453+Jsebastian96@users.noreply.github.com>
2024-07-20 18:07:18 -0500

    commit 12
    filtros y nuevos estudiantes

--- src/app.js
+++ src/app.js
@@ -13,8 +13,11 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
+// Aumentar el límite de carga
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 // Middleware
-app.use(express.json());
 app.use(cors());
 
 // Importar rutas
--- src/routes/students.js
+++ src/routes/students.js
@@ -1,19 +1,30 @@
-// routes/students.js
 const express = require('express');
 const router = express.Router();
 const Student = require('../models/student');
 
-// Obtener estudiantes con paginación
+
+
+// Obtener estudiantes con paginación y filtros
 router.get('/', async (req, res) => {
   const page = parseInt(req.query.pageNumber) || 1;
   const limit = parseInt(req.query.pageSize) || 10;
+  const searchQuery = req.query.searchQuery || '';
+  const sortOption = req.query.sortOption || 'nombre_name';
 
   try {
-    const students = await Student.find()
+    const query = {
+      $or: [
+        { nombre_name: { $regex: searchQuery, $options: 'i' } },
+        { apellido: { $regex: searchQuery, $options: 'i' } }
+      ]
+    };
+
+    const students = await Student.find(query)
+      .sort({ [sortOption]: 1 })
       .skip((page - 1) * limit)
       .limit(limit)
-      .select('-photo_estudiante'); 
-    const totalStudents = await Student.countDocuments();
+      .select('-photo_estudiante');
+    const totalStudents = await Student.countDocuments(query);
 
     res.json({
       students,
@@ -25,6 +36,8 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: 'Error fetching students' });
   }
 });
+
+
 
 // Obtener la foto de un estudiante por ID
 router.get('/:id/photo', async (req, res) => {
@@ -38,9 +51,6 @@ router.get('/:id/photo', async (req, res) => {
     res.status(500).send(error.message);
   }
 });
-
-module.exports = router;
-
 
 // Obtener un estudiante por ID
 router.get('/:id', async (req, res) => {
@@ -65,6 +75,26 @@ router.put('/:id', async (req, res) => {
     res.json(student);
   } catch (error) {
     res.status(500).send(error.message);
+  }
+});
+
+// Crear un nuevo estudiante
+router.post('/', async (req, res) => {
+  try {
+    const { nombre_name, apellido, numero_documento, programa_id, photo_estudiante } = req.body;
+    const newStudent = new Student({
+      nombre_name,
+      apellido,
+      numero_documento,
+      programa_id,
+      photo_estudiante
+    });
+
+    await newStudent.save();
+    res.status(201).json(newStudent);
+  } catch (error) {
+    console.error('Error registering student:', error);
+    res.status(500).json({ error: 'Error registering student' });
   }
 });
 
