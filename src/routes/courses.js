ce20fdbd8cde130ea69267d84572fcc3582479eb
Jsebastian96 <89179453+Jsebastian96@users.noreply.github.com>
2024-07-22 16:45:08 -0500

    commit 17
    delete student

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -44,4 +44,43 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+// Obtener la lista de cursos con inscripciones
+router.get('/', async (req, res) => {
+  const { pageNumber = 1, pageSize = 10 } = req.query;
+  const skip = (pageNumber - 1) * pageSize;
+
+  try {
+    const courses = await Course.find().lean().skip(skip).limit(parseInt(pageSize));
+    const totalCourses = await Course.countDocuments();
+
+    const coursesWithEnrollments = await Promise.all(
+      courses.map(async (course) => {
+        const enrollments = await Enrollment.find({ materia_id: course._id }).lean();
+
+        // Obtener los nombres y apellidos de los estudiantes
+        const studentIds = enrollments.map(enrollment => enrollment.estudiante_id);
+        const students = await Student.find({ _id: { $in: studentIds } });
+
+        const enrollmentsInfo = enrollments.map(enrollment => {
+          const student = students.find(student => student._id.equals(enrollment.estudiante_id));
+          return {
+            nombre: student ? student.nombre_name : 'Desconocido',
+            apellido: student ? student.apellido : 'Desconocido'
+          };
+        });
+
+        const enrollmentsCount = enrollmentsInfo.length;
+
+        return { ...course, enrollmentsCount, enrollmentsInfo };
+      })
+    );
+
+    console.log('Courses with enrollments:', coursesWithEnrollments); 
+    res.json({ courses: coursesWithEnrollments, totalCourses });
+  } catch (error) {
+    console.error('Error fetching courses:', error);
+    res.status(500).send(error.message);
+  }
+});
+
 module.exports = router;
